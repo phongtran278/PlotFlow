3a137e11c18d981a93c2d6c14b4377170629da65
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-17 19:27:56 +0700

    Mount load feedback meter

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import UnitReviewBar from "./components/UnitReviewBar.jsx";
+import PerformanceFeedback from "./components/PerformanceFeedback.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <>
       <App />
       <UnitReviewBar />
+      <PerformanceFeedback />
     </>
   </StrictMode>
 );
