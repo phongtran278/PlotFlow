24d3736750a1b8a66b1e7db92a09be984164c102
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-17 23:24:41 +0700

    Load free camera canvas styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./components/WorkspaceCamera.css";
 import App from "./App.jsx";
 import UnitReviewBar from "./components/UnitReviewBar.jsx";
 import PerformanceFeedback from "./components/PerformanceFeedback.jsx";
