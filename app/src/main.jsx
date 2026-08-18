f3f9df205056ee7a76ff5270ed3590b6521f4528
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-18 20:02:04 +0700

    Stop auto-loading bundled masterplan

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,7 +5,6 @@ import UnitReviewBar from "./components/UnitReviewBar.jsx";
 import PerformanceFeedback from "./components/PerformanceFeedback.jsx";
 import WorkspaceController from "./components/WorkspaceController.jsx";
 import PinScaleControl from "./components/PinScaleControl.jsx";
-import AutoFloorplanSource from "./components/AutoFloorplanSource.jsx";
 import { installPreviewInteractions } from "./previewInteractions.js";
 
 installPreviewInteractions();
@@ -13,7 +12,6 @@ installPreviewInteractions();
 createRoot(document.getElementById("root")).render(
   <>
     <App />
-    <AutoFloorplanSource />
     <UnitReviewBar />
     <PerformanceFeedback />
     <WorkspaceController />
