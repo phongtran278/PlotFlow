ed38722f1391d32fa584b523805bfbd613f0b911
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-18 23:19:47 +0700

    Enable adaptive memory governor

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,14 +6,26 @@ import PerformanceFeedback from "./components/PerformanceFeedback.jsx";
 import WorkspaceController from "./components/WorkspaceController.jsx";
 import PinScaleControl from "./components/PinScaleControl.jsx";
 import AutoFloorplanSource from "./components/AutoFloorplanSource.jsx";
+import MemoryGovernor from "./components/MemoryGovernor.jsx";
+import { installMemoryProfile } from "./runtime/memoryProfile.js";
 import { installPreviewInteractions } from "./previewInteractions.js";
 
-installPreviewInteractions();
+installMemoryProfile();
+
+window.__plotflowPreviewInteractionsCleanup?.();
+window.__plotflowPreviewInteractionsCleanup = installPreviewInteractions();
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => {
+    window.__plotflowPreviewInteractionsCleanup?.();
+    window.__plotflowPreviewInteractionsCleanup = null;
+  });
+}
 
 createRoot(document.getElementById("root")).render(
   <>
     <App />
     <AutoFloorplanSource />
+    <MemoryGovernor />
     <UnitReviewBar />
     <PerformanceFeedback />
     <WorkspaceController />
