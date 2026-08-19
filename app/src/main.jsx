1dae13e821019073185dd5e5b258f334914d72ac
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-19 22:18:32 +0700

    Wire navigator geometry bridge

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,6 +5,7 @@ import UnitReviewBar from "./components/UnitReviewBar.jsx";
 import PerformanceFeedback from "./components/PerformanceFeedback.jsx";
 import WorkspaceController from "./components/WorkspaceController.jsx";
 import WorkspaceGeometryFix from "./components/WorkspaceGeometryFix.jsx";
+import NavigatorGeometryBridge from "./components/NavigatorGeometryBridge.jsx";
 import PinScaleControl from "./components/PinScaleControl.jsx";
 import AutoFloorplanSource from "./components/AutoFloorplanSource.jsx";
 import MemoryGovernor from "./components/MemoryGovernor.jsx";
@@ -33,6 +34,7 @@ createRoot(document.getElementById("root")).render(
     <PerformanceFeedback />
     <WorkspaceController />
     <WorkspaceGeometryFix />
+    <NavigatorGeometryBridge />
     <PinScaleControl />
   </>
 );
