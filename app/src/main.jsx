534997c01ed6c76b2d730e7efeec0aafa0d7292b
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-22 14:39:39 +0700

    Mount final Overview UX and PNG export runtime

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -18,6 +18,7 @@ import EmptyWorkspaceEnhancer from "./product/EmptyWorkspaceEnhancer.jsx";
 import OverviewZoomRuntime from "./product/OverviewZoomRuntime.jsx";
 import OverviewPdfRuntime from "./product/OverviewPdfRuntime.jsx";
 import OverviewExportRuntime from "./product/OverviewExportRuntime.jsx";
+import OverviewPngExportRuntime from "./product/OverviewPngExportRuntime.jsx";
 import OverviewAnchorRuntime from "./product/OverviewAnchorRuntime.jsx";
 import OverviewLiveUnitsRuntime from "./product/OverviewLiveUnitsRuntime.jsx";
 import OverviewDetailLocatorBridge from "./product/OverviewDetailLocatorBridge.jsx";
@@ -27,6 +28,7 @@ import OverviewSellDataRuntime from "./product/OverviewSellDataRuntime.jsx";
 import OverviewPenRuntime from "./product/OverviewPenRuntime.jsx";
 import OverviewSimplifiedRuntime from "./product/OverviewSimplifiedRuntime.jsx";
 import OverviewLayoutPresetRuntime from "./product/OverviewLayoutPresetRuntime.jsx";
+import OverviewFinalUxRuntime from "./product/OverviewFinalUxRuntime.jsx";
 import "./product/OverviewCallouts.css";
 import "./components/ToolbarConsistency.css";
 import "./components/AuroraTheme.css";
@@ -68,6 +70,7 @@ createRoot(document.getElementById("root")).render(
       <OverviewZoomRuntime />
       <OverviewPdfRuntime />
       <OverviewExportRuntime />
+      <OverviewPngExportRuntime />
       <OverviewAnchorRuntime />
       <OverviewLiveUnitsRuntime />
       <OverviewDetailLocatorBridge />
@@ -76,6 +79,7 @@ createRoot(document.getElementById("root")).render(
       <OverviewPenRuntime />
       <OverviewSimplifiedRuntime />
       <OverviewLayoutPresetRuntime />
+      <OverviewFinalUxRuntime />
     </ProductShell>
   </AuthGate>
 );
