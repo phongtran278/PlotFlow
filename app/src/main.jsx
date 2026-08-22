92545b43939d72703da57dca40a549fa09b21e11
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-08-22 16:25:35 +0700

    Mount workspace home return styling

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/CommercialShell.css";
+import "./components/WorkspaceReturn.css";
 import "./product/overviewV2Migration.js";
 import App from "./App.jsx";
 import HomeLanding from "./components/HomeLanding.jsx";
