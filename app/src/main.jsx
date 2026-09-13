1708ceb6c4f72b1fe873655c08260a199215bf09
UIT.23730208 <58768498+phongtran278@users.noreply.github.com>
2026-09-13 09:17:10 +0700

    Wire deterministic Auto Arrange solver

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -36,7 +36,7 @@ import OverviewDetailLocatorBridge from "./product/OverviewDetailLocatorBridge.j
 import OverviewControlRailRuntime from "./product/OverviewControlRailRuntime.jsx";
 import OverviewPrecisionArrangeRuntime from "./product/OverviewPrecisionArrangeRuntime.jsx";
 import OverviewV2Runtime from "./product/OverviewV2Runtime.jsx";
-import OverviewArrangeModesRuntime from "./product/OverviewArrangeModesRuntime.jsx";
+import OverviewArrangeLogicRuntime from "./product/OverviewArrangeLogicRuntime.jsx";
 import OverviewSellDataRuntime from "./product/OverviewSellDataRuntime.jsx";
 import OverviewPenRuntime from "./product/OverviewPenRuntime.jsx";
 import OverviewHighlightPointEditRuntime from "./product/OverviewHighlightPointEditRuntime.jsx";
@@ -137,7 +137,7 @@ function OverviewMasterplanEngine() {
 }
 
 function OverviewRuntimes() {
-  return <><OverviewZoomRuntime/><WindowsOverviewViewportRuntime/><OverviewMasterplanEngine/><OverviewExportRuntime/><OverviewHeaderExportRuntime/><OverviewAnchorRuntime/><OverviewLiveUnitsRuntime/><OverviewDetailLocatorBridge/><OverviewControlRailRuntime/><OverviewPrecisionArrangeRuntime/><OverviewV2Runtime/><OverviewArrangeModesRuntime/><OverviewPenRuntime/><OverviewHighlightPointEditRuntime/><OverviewSimplifiedRuntime/><OverviewGuideRuntime/><OverviewInteractionRuntime/><OverviewLayerRevealRuntime/><OverviewUnitBadgeRuntime/></>;
+  return <><OverviewZoomRuntime/><WindowsOverviewViewportRuntime/><OverviewMasterplanEngine/><OverviewExportRuntime/><OverviewHeaderExportRuntime/><OverviewAnchorRuntime/><OverviewLiveUnitsRuntime/><OverviewDetailLocatorBridge/><OverviewControlRailRuntime/><OverviewPrecisionArrangeRuntime/><OverviewV2Runtime/><OverviewArrangeLogicRuntime/><OverviewPenRuntime/><OverviewHighlightPointEditRuntime/><OverviewSimplifiedRuntime/><OverviewGuideRuntime/><OverviewInteractionRuntime/><OverviewLayerRevealRuntime/><OverviewUnitBadgeRuntime/></>;
 }
 
 function WorkspaceAuxiliaryRuntimes() {
